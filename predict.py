471fd0416d66bfd340a4e8796a8a6766f13af222
IsseiFUJIMOTO <i.fujimoto@accelia.net>
2018-05-18 14:57:28 +0900

    文章生成までの予測を追加

--- predict.py
+++ predict.py
@@ -1,16 +1,45 @@
 
-from model.mnist import Mnist
+
+from model.char_autoencoder import CharAutoencoder
+
 from preprocessing.preprocessing import PreProcessing
-from model_exec.learning import Learning
 from model_exec.predict import Predict
+import numpy as np
+
+import sys
+sys.path.append("../")
+# from string2image.image2string import image2string
+from string2image.image2string import Image2String
+from cnn_autoencoder.model.simple_autoencoder import SimpleAutoencoder
+# from cnn_autoencoder.model_exec.predict import Predict
+
 
 def main():
-    mnist_model = Mnist().load_model()
-    test_x, test_y = PreProcessing().make_test_data()
-    score = Predict.run(mnist_model, test_x, test_y)
+    char_model = CharAutoencoder().load_model()
 
-    print('Test loss:', score[0])
-    print('Test accuracy:', score[1])
+    feature_list = PreProcessing().make_test_data("あ")
+    loop = True
+    while(loop):
+        predict_feature_list = Predict.run(char_model, feature_list)
+        last_feature = np.array(
+            predict_feature_list[0][-1]).reshape(1, 1, 128)
+        feature_list = np.append(feature_list, last_feature, axis=1)
+        if feature_list.shape[1] == 5:
+            loop = False
+
+    cnn_model = SimpleAutoencoder().load_model("cnn_model.hdf5")
+    decoder_model = SimpleAutoencoder().make_decoder_model(cnn_model)
+
+    sentence = ""
+
+    for feature in feature_list[0]:
+        feature = feature.reshape(1, 4, 4, 8)
+        img = Predict.run(decoder_model, feature)
+        char = Image2String.image2string(img)
+        print(char)
+        sentence += char
+    print(sentence)
+
 
 if __name__ == '__main__':
-   main()
+    main()
