533e3a383efbb39298a600b5b8b5a4b6262e70bb
IsseiFUJIMOTO <i.fujimoto@accelia.net>
2018-05-21 13:57:09 +0900

    add dropout

--- predict.py
+++ predict.py
@@ -50,33 +50,6 @@ def test():
     cnn_model = SimpleAutoencoder().load_model("cnn_model.hdf5")
     decoder_model = SimpleAutoencoder().make_decoder_model(cnn_model)
 
-    test_sentence = "ロマンチックな蝋燭の光が、その"
-    feature_list = []
-    for char in test_sentence:
-        tmp = PreProcessing().make_test_data(char)
-        feature_list.append(list(tmp))
-
-    feature_list = np.array(feature_list).reshape(1, len(test_sentence), 128)
-    print(feature_list.shape)
-
-    loop = True
-    while(loop):
-        predict_feature_list = Predict.run(char_model, feature_list)
-        last_feature = np.array(
-            predict_feature_list[0][-1]).reshape(1, 1, 128)
-        feature_list = np.append(feature_list, last_feature, axis=1)
-        if feature_list.shape[1] >= 20:
-            loop = False
-
-    sentence = ""
-    for feature in feature_list[0]:
-        feature = feature.reshape(1, 4, 4, 8)
-        img = Predict.run(decoder_model, feature)
-        char = Image2String.image2string(img)
-        print(char)
-        sentence += char
-    print(sentence)
-
     test_sentence = "今晩の話手と定められた新入"
     feature_list = []
     for char in test_sentence:
