3c452ad388470ff738d4d1afbd4d3002c44a9e5c
Issei Fujimoto <otomijuf.004@gmail.com>
2018-05-21 20:54:42 +0900

    np load save 使ってみる

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -75,13 +75,52 @@ class PreProcessing(abc_preprocessing.ABCPreProcessing):
         return train_data, teach_data
 
     @classmethod
-    def make_test_data(cls, char):
+    def save_train_data(cls, data_size, window_size=5):
         autoencoder = SimpleAutoencoder.load_model("cnn_model.hdf5")
         encoder = SimpleAutoencoder.make_encoder_model(autoencoder)
-        feature = get_feature.char2feature(char, encoder)
-        feature = feature.reshape(1, 1, 4 * 4 * 8)
-        print(feature.shape)
-        return feature
+
+        word_list = PreProcessing.__get_word_lists(
+            "../aozora_data/files/files_all_rnp.txt")
+
+        from itertools import chain
+        word_list = list(chain.from_iterable(word_list))
+        word_list = "".join(word_list)
+
+        window_sentence = []
+        train_data = []
+        teach_data = []
+        rand_num = rand.randint(0, len(word_list) - data_size + 1)
+
+        for char in word_list[rand_num: rand_num + data_size]:
+            sys.stdout.write("\r now:(%d/%d)" % (len(train_data), data_size))
+            sys.stdout.flush()
+            feature = get_feature.char2feature(char, encoder)
+            feature = feature.reshape(4 * 4 * 8)
+            window_sentence.append(feature)
+            if len(window_sentence) == window_size + 1:
+                train_data.append(window_sentence[:-1])
+                teach_data.append(window_sentence[1:])
+                window_sentence = window_sentence[1:]
+        print("")
+        train_data = np.array(train_data)
+        teach_data = np.array(teach_data)
+        print("train shape:", train_data.shape)
+        print("teach shape:", teach_data.shape)
+        print("save")
+        np.save("./train",train_data)
+        np.save("./teach",teach_data)
+
+    @classmethod
+    def load_train_data(cls):
+        train_data = np.load("./train.npy")
+        teach_data = np.load("./teach.npy")
+        print("train shape:", train_data.shape)
+        print("teach shape:", teach_data.shape)
+        return train_data,teach_data
+    
+    @classmethod
+    def make_test_data(cls):
+        pass
 
 
 def main():
