4649cbb7b902b979153ad906abb58d8efbf0aaa4
IsseiFUJIMOTO <i.fujimoto@accelia.net>
2018-05-21 17:11:41 +0900

    resume修正

--- train.py
+++ train.py
@@ -8,13 +8,14 @@ from model.config import Config
 data_size = 60000
 test_size = 10000
 
+
 def main():
     train, teach = PreProcessing().make_train_data(data_size, window_size=25)
     test_train, test_teach = PreProcessing().make_train_data(test_size, window_size=25)
     char_model = CharAutoencoder().make_simple_model()
     cbs = CharAutoencoder().set_callbacks(Config.save_model)
-    hist = Learning.run_with_test(char_model, train, teach, test_train, test_teach, cbs)
-    CharAutoencoder().save_model(char_model)
+    hist = Learning.run_with_test(char_model, train, teach, test_train, test_teach, cbs)t
+    CharAutoencoder().save_model(char_model)n
 
 
 if __name__ == '__main__':
