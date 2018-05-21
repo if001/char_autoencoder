5d99d7393b515aa9da29b76dc3ea857f3201e53b
IsseiFUJIMOTO <i.fujimoto@accelia.net>
2018-05-21 17:05:09 +0900

    add cython

--- train-resume.py
+++ train-resume.py
@@ -5,25 +5,18 @@ from preprocessing.preprocessing import PreProcessing
 from model_exec.learning import Learning
 from model_exec.predict import Predict
 from model.config import Config
-data_size = 100
+data_size = 60000
+test_size = 10000
 
 
 def main():
-    train, teach = PreProcessing().make_train_data(data_size, window_size=10)
+    train, teach = PreProcessing().cy_make_train_data(data_size, window_size=10)
+    test_train, test_teach = PreProcessing().cy_make_train_data(test_size, window_size=25)
     char_model = CharAutoencoder().load_model()
     cbs = CharAutoencoder().set_callbacks(Config.save_model)
-    hist = Learning.run(char_model, train, teach, cbs)
+    hist = Learning.run_with_test(
+        char_model, train, teach, test_train, test_teach, cbs)
     CharAutoencoder().save_model(char_model)
-
-    # train_x, train_y = PreProcessing().make_train_data()
-    # mnist_model = Mnist().load_model()
-    # hist = Learning.run(mnist_model, train_x, train_y)
-
-    # test_x, test_y = PreProcessing().make_test_data()
-    # score = Predict.run(mnist_model, test_x, test_y)
-
-    # print('Test loss:', score[0])
-    # print('Test accuracy:', score[1])
 
 
 if __name__ == '__main__':
