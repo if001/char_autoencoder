3b6abc25a1a2ba1161cb69436bb18927888b8dbc
IsseiFUJIMOTO <i.fujimoto@accelia.net>
2018-05-22 10:28:46 +0900

    edit

--- train-resume.py
+++ train-resume.py
@@ -1,5 +1,3 @@
-
-
 from model.char_autoencoder import CharAutoencoder
 from preprocessing.preprocessing_mod import PreProcessing
 from model_exec.learning import Learning
@@ -10,12 +8,15 @@ test_size = 10000
 
 
 def main():
-    train, teach = PreProcessing().make_train_data(data_size, window_size=25)
-    test_train, test_teach = PreProcessing().make_train_data(test_size, window_size=25)
+    # train, teach = PreProcessing().make_train_data(data_size, window_size=25)
+    # test_train, test_teach = PreProcessing().make_train_data(test_size, window_size=25)
+    train, teach = PreProcessing().load_train_data()
+
     char_model = CharAutoencoder().load_model()
     cbs = CharAutoencoder().set_callbacks(Config.save_model)
-    hist = Learning.run_with_test(
-        char_model, train, teach, test_train, test_teach, cbs)
+    hist = Learning.run(char_model, train, teach, cbs)
+    # hist = Learning.run_with_test(
+    #     char_model, train, teach, test_train, test_teach, cbs)
     CharAutoencoder().save_model(char_model)
 
 
